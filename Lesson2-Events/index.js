1273c1a65f80620186b77525debc814ce987b357
Wesley Wanga <wesleyjwanga@gmail.com>
2026-04-28 20:32:14 +0300

    update the files

--- Lesson2-Events/index.js
+++ Lesson2-Events/index.js
@@ -8,14 +8,31 @@ const resetButton=document.querySelector("#reset-bin")
 nameInput.addEventListener("input", function(){
     preview.textContent="You are typing: " + nameInput.ariaValueMax;
 })
-form.addEventListener("submit" ,function(event){
+form.addEventListener("submit",function(event){
     event.preventDefault();
-    message.textContent="Welcome " + nameInput.value
-
+   const studentName=nameInput.value;
+    fetch ("https://jsonplaceholder.typicode.com/users/2") 
+   .then(function(response){
+    return response.json();
+    })
+    then(function(data){
+        message.textContent=
+        "Welcome" + studentName + "API user:" + data.name + "Email: " + data.email + 
+        "Phone Number :"+ data.phone + "Company" + data.company.name
+    console.log(data)
+})
 })
 
 resetButton.addEventListener("click", function(){
     nameInput.value="";
     preview.textContent="Preview  will appear here....."
     message.textContent=" ";
+})
+
+fetch ("https://jsonplaceholder.typicode.com/users/1") 
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data)
 })
